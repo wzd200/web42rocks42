39a42ad9ea13eeee75a3cc0ea67143b0d106b867
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-06-03 13:17:35 -0400

    deployable?

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const server = express()
+
+console.log(process.env.USER)
+console.log(process.env.SHELL)
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('this means this code is deployed')
+}
+
+const PORT = process.env.PORT || 5000
+
+console.log('port is =>', PORT)
+
+server.get('/api', (req, res) => {
+    res.json({ message: `${process.env.COHORT} ROCKS!` })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
