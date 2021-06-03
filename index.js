98e0a571f860effe2db0fbc7f60358b9a4efa2eb
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-06-03 16:40:54 -0400

    WIP

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
 const server = express()
 
-console.log(process.env.USER)
-console.log(process.env.SHELL)
+server.use(express.json())
+server.use(cors())
+server.use(express.static(path.join(__dirname, 'client/build'))) //static assets
 
-//test
+console.log(process.env.USER) // env USER=williamdye
+console.log(process.env.SHELL) // env SHELL=/bin/zsh
+
+//test test
 
 if (process.env.NODE_ENV === 'production') {
     console.log('this means this code is deployed')
@@ -14,6 +21,10 @@ if (process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000
 
 console.log('port is =>', PORT)
+
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
 
 server.get('/api', (req, res) => {
     res.json({ message: `${process.env.COHORT} ROCKS!` })
